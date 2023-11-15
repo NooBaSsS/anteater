54fc6567775e24e9d7f27c17e15403ff47d8281a
NooBaSsS <90272720+NooBaSsS@users.noreply.github.com>
2023-11-15 21:48:09 +0300

    Add files via upload

--- anteater/main.py
+++ anteater/main.py
@@ -1,16 +1,21 @@
 COLS = 5
 ROWS = 5
 
+
 class Field:
     def __init__(self) -> None:
         self.rows = ROWS
         self.cols = COLS
         self.cells = [[Cell(y, x) for x in range(self.cols)] for y in range(self.rows)]
+        self.player = None
 
     def draw(self) -> None:
         for row in self.cells:
             for cell in row:
-                print(cell, end=' ')
+                if self.player is not None and cell.y == self.player.y and cell.x == self.player.x:
+                    print(player, end=' ')  # Player(image='P') - игрок
+                else:
+                    print(cell, end=' ')
             print()
 
 
@@ -24,5 +29,18 @@ class Cell:
         return self.image
 
 
+class Player:
+    def __init__(self, field, y, x) -> None:
+        self.y = y
+        self.x = x
+        self.field = field
+        self.field.player = self
+        self.image = 'P'
+
+    def __str__(self) -> str:
+        return self.image
+
+
 field = Field()
+player = Player(field, 0, 0) # y и x игрока
 field.draw()
